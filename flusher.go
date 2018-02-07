20ad32dc90cda141e6c909ba63de8a797c1542b9
Andreas Fuchs <asf@stripe.com>
2018-02-07 16:50:17 +0100

    Make flushing events & service checks parallel
    This ensures the flushing routine gets the context with the right
    deadline set, and flushes all metric sinks in parallel. This should
    not only speed up flushing with multiple metrics providers defined, it
    should also add resiliency to them partially-failing.

--- flusher.go
+++ flusher.go
@@ -34,6 +34,26 @@ func (s *Server) flushMetricSinks(ctx context.Context, finalMetrics []samplers.I
 	wg.Wait()
 }
 
+func flushEventSink(ctx context.Context, wg *sync.WaitGroup, sink sinks.MetricSink, events []samplers.UDPEvent, checks []samplers.UDPServiceCheck) {
+	sink.FlushEventsChecks(ctx, events, checks)
+	wg.Done()
+}
+
+func (s *Server) flushEventChecks(ctx context.Context, events []samplers.UDPEvent, checks []samplers.UDPServiceCheck) {
+	if s.MetricFlushTimeout > 0 {
+		var cancel func()
+		ctx, cancel = context.WithTimeout(ctx, s.MetricFlushTimeout)
+		defer cancel()
+	}
+
+	wg := sync.WaitGroup{}
+	for _, sink := range s.metricSinks {
+		wg.Add(1)
+		go flushEventSink(ctx, &wg, sink, events, checks)
+	}
+	wg.Wait()
+}
+
 // Flush collects sampler's metrics and passes them to sinks.
 func (s *Server) Flush(ctx context.Context) {
 	span := tracer.StartSpan("flush").(*trace.Span)
@@ -51,10 +71,7 @@ func (s *Server) Flush(ctx context.Context) {
 	s.Statsd.Count("worker.events_flushed_total", int64(len(events)), nil, 1.0)
 	s.Statsd.Count("worker.checks_flushed_total", int64(len(checks)), nil, 1.0)
 
-	// TODO Concurrency
-	for _, sink := range s.metricSinks {
-		sink.FlushEventsChecks(span.Attach(ctx), events, checks)
-	}
+	s.flushEventChecks(ctx, events, checks)
 
 	go s.flushTraces(span.Attach(ctx))
 
