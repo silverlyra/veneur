16a713b3b491e4a26a98070ab635662a40c63931
Andreas Fuchs <asf@stripe.com>
2018-02-07 15:44:02 +0100

    Add a test for datadog metrics flush timeouts

--- sinks/datadog/datadog_test.go
+++ sinks/datadog/datadog_test.go
@@ -277,3 +277,37 @@ func TestDatadogMetricRouting(t *testing.T) {
 	}
 
 }
+
+func TestTimeoutsOnMetrics(t *testing.T) {
+	stats, _ := statsd.NewBuffered("localhost:1235", 1024)
+	client := &http.Client{Transport: &http.Transport{DisableCompression: true}}
+	srv, rcved := ddTestServer(t, "/api/v1/series", "never.actually.acked")
+	defer close(rcved)
+
+	ddSink := DatadogMetricSink{
+		DDHostname:      srv.URL,
+		HTTPClient:      client,
+		flushMaxPerBody: 15,
+		log:             logrus.New(),
+		tags:            []string{"a:b", "c:d"},
+		interval:        10,
+		statsd:          stats,
+	}
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Nanosecond)
+	defer cancel()
+
+	// A failure to consider the context should result in a test
+	// timeout:
+	ddSink.Flush(ctx, []samplers.InterMetric{
+		samplers.InterMetric{
+			Name:      "never.actually.acked",
+			Timestamp: time.Now().Unix(),
+			Value:     float64(10),
+			Tags:      []string{"gorch:frobble", "x:e"},
+			Type:      samplers.CounterMetric,
+			Sinks:     samplers.RouteInformation{},
+		},
+	})
+}
